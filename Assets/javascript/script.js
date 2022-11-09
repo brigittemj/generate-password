1b203a50fb340653ac37a2daac5732ad5d8601d5
Brigitte <brigitte@Brigittes-MacBook-Air.local>
2022-11-09 15:46:20 -0600

    added choices variables

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uppercase = ['a','b','c','d','e','f','g','h','i','j','k'];
+var lowercase = ['A','B','C','D','E','F','G','H','I','J','K'];
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+var specialCharacters = ['!','@','#','$','%','^','&','*','(',')'];
 
 // Write password to the #password input
 function writePassword() {
@@ -9,11 +13,12 @@ function writePassword() {
   passwordText.value = password;
 
 }
-function generatePassword() {
-  password = prompt("Input a password of greater than 8 characters and less than 128 characters")
 
-  if (password.length <= 8) {
-    alert("password must be greater than 8 characters")
+function generatePassword() {
+  var passwordLength = prompt("Input a password of greater than 8 characters and less than 128 characters");
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("password must be greater than 8 characters and less than 128 characters");
   }
 }
 
